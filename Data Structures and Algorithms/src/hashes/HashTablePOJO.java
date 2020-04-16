8a756e761d316911ad01ba980988f6ec8184d5c0
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-04-15 19:13:54 -0500

    added hasKey hasValue and delete method for hash.

--- Data Structures and Algorithms/src/hashes/HashTablePOJO.java
+++ Data Structures and Algorithms/src/hashes/HashTablePOJO.java
@@ -37,6 +37,62 @@ public class HashTablePOJO<X,Y> {
 		size++;
 	}
 	
+	public Y delete(X key) {
+		// first get the value for this key so it can be return
+		Y value = get(key);
+		
+		// clear out the hashtable slot for the key and return the removed value
+		if(value !=null) {
+			int hash = calculateHash(key);
+			data[hash] = null;
+			size--;
+			hash = (hash + 1) % this.capacity;
+			
+			// if the next slot isn't empty we should re add it so we can keep the hash algorithms clean
+			while(data[hash] != null) {
+				HashEntry he = data[hash];
+				data[hash] = null;
+				put( (X)he.getKey(), (Y)he.getValue() );
+				
+				// we repositioned the hash item and didnt really add a new one so we back off the size
+				size--;
+				hash = (hash + 1) % this.capacity;
+			}
+			
+		}
+		return value;
+	}
+	
+	public boolean hasKey(X key) {
+		int hash = calculateHash(key);
+		
+		// if we dont have anything for the given key, just return false
+		if(data[hash] == null) {
+			return false;
+		}
+		// otherwise get the hash entry for the key and see if it matches the given key
+		else {
+			if(data[hash].getKey().equals(key)) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	public boolean hasValue(Y value) {
+		// loop through all of the hash entries
+		for (int i = 0; i < this.capacity; i++) {
+			HashEntry entry = data[i];
+			
+			// if this hash entry isn't null and the value equals the one passed in, the hash table has this value.
+			if (entry != null && entry.getValue().equals(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	private int calculateHash(X key) {
 		int hash = (key.hashCode() % this.capacity);
 		
