0a47ca64e1f0b42f6c4eca01c12e46483fe95e33
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-04-22 00:04:30 -0500

    added delete method in binary tree pojo and added contact manager app to check if binary tree pojo methods work.

--- Data Structures and Algorithms/src/tree/BinaryTreePOJO.java
+++ Data Structures and Algorithms/src/tree/BinaryTreePOJO.java
@@ -64,6 +64,106 @@ public class BinaryTreePOJO<X extends Comparable<X>> {
 		}
 		
 	}
+	
+	public boolean contains(X item) {
+		Node currentNode = getNode(item);
+		
+		if (currentNode == null) {
+			// didn't find it
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean delete(X item) {
+		boolean deleted = false;
+		
+		// make sure the root isn't null meaning the tree is empty
+		if (this.root == null) {
+			return false;
+		}
+		
+		// find the node to delete
+		Node currentNode = getNode(item);
+		
+		if (currentNode != null) {
+			// if the node to delete doesn't have any children, just delete it
+			if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+				unlink(currentNode, null);
+				deleted = true;
+			}
+			// if the node to delete only has a right child, remove it in the hierarchy
+			else if (currentNode.getLeft() == null && currentNode.getRight() != null) {
+				unlink(currentNode, currentNode.getRight());
+				deleted = true;
+			}
+			// if the node to delete only has a left child, remove it in the hierarchy
+			else if (currentNode.getLeft() != null && currentNode.getRight() == null) {
+				unlink(currentNode, null);
+				deleted = true;
+			}
+			// the node has both children, do a node swap delete
+			else {
+				// you can swap out the node with the right most leaf node on the left side
+				Node child = currentNode.getLeft();
+				while (child.getRight() != null && child.getLeft() != null) {
+					child = child.getRight();
+				}
+				
+				// we have the right most node. we can replace the current node with this node
+				child.getParent().setRight(null); // remove the leaf node from it's current position
+				
+				child.setLeft(currentNode.getLeft());
+				child.setRight(currentNode.getRight());
+				
+				unlink(currentNode, child);
+				deleted = true;
+			}
+		}
+		if (deleted) {
+			this.size--;
+		}
+		
+		return deleted;
+	}
+	
+	private void unlink(Node currentNode, Node newNode) {
+		// if this is the root node, we replace that a little differently
+		if (currentNode == this.root) {
+			newNode.setLeft(currentNode.getLeft());
+			newNode.setRight(currentNode.getRight());
+			this.root = newNode;
+		}
+		else if (currentNode.getParent().getRight() == currentNode) {
+			currentNode.getParent().setRight(newNode);
+		}
+		else {
+			currentNode.getParent().setLeft(newNode);
+		}
+	}
+	
+	private Node getNode(X item) {
+		Node currentNode = this.root;
+		
+		while ( currentNode != null ) {
+			int val = item.compareTo(currentNode.getItem());
+			
+			// see if the node is a match
+			if (val == 0) {
+				return currentNode;
+			}
+			// if the val is less than 0 we got to the left side of the tree
+			else if (val < 0) {
+				currentNode = currentNode.getLeft();
+			}
+			// otherwise we got to the right side
+			else {
+				currentNode = currentNode.getRight();
+			}
+		}
+		return null;
+	}
 
 	private class Node {
 		private Node left;
